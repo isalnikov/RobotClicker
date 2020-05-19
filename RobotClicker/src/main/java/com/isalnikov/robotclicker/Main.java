5a24916f5e1fbfd95003da54957197de747fffcf
igor salnikov <>
2020-05-19 22:59:14 +0300

    add frame

--- RobotClicker/src/main/java/com/isalnikov/robotclicker/Main.java
+++ RobotClicker/src/main/java/com/isalnikov/robotclicker/Main.java
@@ -1,10 +1,33 @@
-
 package com.isalnikov.robotclicker;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        log.info("Hello");
+        createWindow();
     }
-    
+
+    private static void createWindow() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Clicker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel textLabel = new JLabel("I'm a label in the window", SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+        //Display the window.
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+
 }
